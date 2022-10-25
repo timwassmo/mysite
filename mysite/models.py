b90eff0b9a26f7f117d642233f8177b7cd90b108
helenehuseby <helene-huseby@hotmail.com>
2022-10-25 11:42:56 +0200

    testkjøring:D

--- mysite/models.py
+++ mysite/models.py
@@ -3,8 +3,20 @@ from django.db import models
 class Car(models.Model):
     make = models.CharField(max_length=50)
     carmodel = models.CharField(max_length=50)
-    year = models.DateField()
+    year = models.IntField()
+    location = models.CharField(max_lenght=50)
+    status = models.CharField(max_lenght=50)
 
 
     def __str__(self):
-        return self.make + ' ' + self.carmodel
+        return self.make + self.year + self.location + self.status + self.carmodel
+
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    branch = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name + self.year + self.branch
+
+
--- mysite/serializers.py
+++ mysite/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Car
+from .models import Car, Employee
 
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
         fields = ['id', 'make', 'carmodel', 'year', 'location', 'status']
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = ['id', 'name', 'address', 'branch']
+
