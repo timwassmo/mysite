3d602f9f17d12e20ad293695fc07fbaaf57f1cb8
helenehuseby <helene-huseby@hotmail.com>
2022-10-25 11:54:34 +0200

    Lagt til Employee

--- mysite/models.py
+++ mysite/models.py
@@ -17,3 +17,10 @@ class Customer(models.Model):
     def __str__(self):
         return self.name + " " + self.age + ", " + self.address
 
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    branch = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name + self.year + self.branch
