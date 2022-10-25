401bfa455a47be79639ed01a17c1c4c878e13cf8
timwassmo <timwassmo@gmail.com>
2022-10-25 11:51:23 +0200

    Lagt til Customer klasse

--- mysite/models.py
+++ mysite/models.py
@@ -3,21 +3,17 @@ from django.db import models
 class Car(models.Model):
     make = models.CharField(max_length=50)
     carmodel = models.CharField(max_length=50)
-    year = models.IntField()
-    location = models.CharField(max_lenght=50)
-    status = models.CharField(max_lenght=50)
+    year = models.DateField()
 
 
     def __str__(self):
-        return self.make + self.year + self.location + self.status + self.carmodel
+        return self.make + ' ' + self.carmodel
 
-class Employee(models.Model):
+class Customer(models.Model):
     name = models.CharField(max_length=50)
+    age = models.IntegerField()
     address = models.CharField(max_length=50)
-    branch = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.name + self.year + self.branch
-
-
+        return self.name + " " + self.age + ", " + self.address
 
