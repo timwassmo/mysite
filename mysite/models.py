7921d9cb429104c097d884fc7a5ad0f7a7f38230
timwassmo <timwassmo@gmail.com>
2022-10-25 11:23:07 +0200

    endret car

--- mysite/models.py
+++ mysite/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Car(models.Model):
     make = models.CharField(max_length=50)
     carmodel = models.CharField(max_length=50)
+    year = models.DateField()
+
 
     def __str__(self):
         return self.make + ' ' + self.carmodel
--- mysite/serializers.py
+++ mysite/serializers.py
@@ -4,4 +4,4 @@ from .models import Car
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
-        fields = ['id', 'make', 'carmodel']
+        fields = ['id', 'make', 'carmodel', 'year', 'location', 'status']
